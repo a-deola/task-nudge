c06b15d99ef47d3fbbfbb9c38bb2fc9ddbc5892d
Adeola Samuel <meetprecious1403@gmail.com>
2024-09-09 22:05:42 +0100

    update next font

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,4 +1,4 @@
-import { Pacifico } from "next/font/google";
+import { Pacifico } from "@next/font/google";
 
 const pacifico = Pacifico({ weight: "400", subsets: ["latin"] });
 interface LogoProps {
